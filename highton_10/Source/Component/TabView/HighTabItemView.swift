22f65e734a33deeffe7c125a5ca60288736c50c4
DGSWcsh <101883606+ChoiSihun@users.noreply.github.com>
2025-02-15 18:34:42 +0900

    feat: ColorExt, HighTabView

---
+++ highton_10/Source/Component/TabView/HighTabItemView.swift
@@ -0,0 +1,43 @@
+//
+//  HighTabItemView.swift
+//  highton_10
+//
+//  Created by 최시훈 on 2/15/25.
+//
+
+import SwiftUI
+
+
+struct HighTabItemView<Content: View>: HighTabItemViewProtocol {
+
+    
+    let title: String
+    let image: Image
+    let content: AnyView
+    
+    init(title: String,
+         image: Image,
+         @ViewBuilder content: () -> Content)
+    {
+        self.title = title
+        self.image = image
+        self.content = AnyView(content())
+    }
+    
+    var body: some View { EmptyView() }
+}
+
+struct HighTabItemModifier: ViewModifier {
+    let title: String
+    let image: Image
+    
+    public func body(content: Content) -> some View {
+        content
+    }
+}
+
+protocol HighTabItemViewProtocol: View {
+    var title: String { get }
+    var image: Image { get }
+    var content: AnyView { get }
+}
---
+++ highton_10/Source/Component/TabView/HighTabViewButton.swift
@@ -0,0 +1,103 @@
+//
+//  HighTabViewButton.swift
+//  highton_10
+//
+//  Created by 최시훈 on 2/15/25.
+//
+
+import SwiftUI
+
+struct HighTabViewButton: View {
+    
+    let label: String
+    let image: Image
+    let action: () -> ()
+    var selected: Bool
+    let haptic: UIImpactFeedbackGenerator.FeedbackStyle?
+    @State var buttonRect: CGRect = CGRect.zero
+    @State var animationState: Int = 0
+    @State var touchdownState: Bool = false
+    @State var rectangleState: Bool = false
+    
+    init(_ label: String,
+                _ image: Image,
+                _ selected: Bool,
+                haptic: UIImpactFeedbackGenerator.FeedbackStyle? = nil,
+                action: @escaping () -> ())
+    {
+        self.label = label
+        self.image = image
+        self.selected = selected
+        self.action = action
+        self.haptic = haptic
+    }
+    
+    var head: some View {
+        ZStack {
+            if rectangleState {
+                Rectangle()
+                    .foregroundColor(Color.hover)
+                    .cornerSmooth(.radius16)
+            }
+            VStack(spacing: 3) {
+                image
+                    .resizable()
+                    .renderingMode(.template)
+                    .scaledToFit()
+                    .frame(width: 24, height: 24)
+                    .scaleEffect(x: animationState == 2 ? 1.1 : 1, y: 1)
+                    .scaleEffect(x: 1, y: animationState == 3 ? 1.05 : 1)
+                Text(label)
+                    .font(.system(size: 11, weight: .medium))
+                    .scaleEffect(animationState == 2 ? 1.05 : 1)
+            }
+            .foregroundColor(selected || touchdownState ? Color.contentBase : Color.contentDisabled)
+        }
+        .frame(width: 56, height: 56)
+        .scaleEffect(animationState == 1 ? 0.9 : 1)
+    }
+    
+    var body: some View {
+        head
+            .background(
+                GeometryReader { (geometry) -> Color in
+                    let rect = geometry.frame(in: .global)
+                    DispatchQueue.main.async {
+                        buttonRect = rect
+                    }
+                    return .clear
+                }
+            )
+            .simultaneousGesture(
+                DragGesture(minimumDistance: 0, coordinateSpace: .global)
+                    .onChanged { newValue in
+                        touchdownState = buttonRect.contains(newValue.location)
+                        rectangleState = true
+                        withAnimation(.linear(duration: 0.1)) {
+                            animationState = 1
+                        }
+                    }
+                    .onEnded { _ in
+                        if touchdownState {
+                            if let haptic = haptic {
+                                UIImpactFeedbackGenerator(style: haptic)
+                                    .impactOccurred()
+                            }
+                            action()
+                        }
+                        for i in 2..<5 {
+                            withAnimation(
+                                .linear(duration: 0.1)
+                                .delay(Double(i - 2)/10*2)
+                            ) {
+                                animationState = i == 4 ? 0 : i
+                                if i == 2 {
+                                    touchdownState = false
+                                    rectangleState = false
+                                }
+                            }
+                        }
+                    }
+            )
+    }
+}
