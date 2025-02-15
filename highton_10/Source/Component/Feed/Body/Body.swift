c71487811b02d8d95378546abfe1837858ee98d3
DGSWcsh <101883606+ChoiSihun@users.noreply.github.com>
2025-02-15 18:54:18 +0900

    add

---
+++ highton_10/Source/Component/Feed/Body/Body.swift
@@ -0,0 +1,8 @@
+//
+//  Body.swift
+//  highton_10
+//
+//  Created by 최시훈 on 2/15/25.
+//
+
+import Foundation
---
+++ highton_10/Source/Component/Feed/Header/Header.swift
@@ -0,0 +1,8 @@
+//
+//  Header.swift
+//  highton_10
+//
+//  Created by 최시훈 on 2/15/25.
+//
+
+import Foundation
