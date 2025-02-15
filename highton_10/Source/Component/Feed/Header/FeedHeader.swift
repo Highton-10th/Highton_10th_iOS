86617497fc4e4e86ff19f0ef291316f64255fe5b
DGSWcsh <101883606+ChoiSihun@users.noreply.github.com>
2025-02-15 20:02:24 +0900

    feat: FeedHeaber

--- highton_10/Source/Component/Feed/Header/FeedHeader.swift
+++ highton_10/Source/Component/Feed/Header/FeedHeader.swift
@@ -1,8 +1,39 @@
 //
-//  Header.swift
+//  FeedHeader.swift
 //  highton_10
 //
 //  Created by 최시훈 on 2/15/25.
 //
 
-import Foundation
+import SwiftUI
+
+struct FeedHeader: View {
+    let profileImage: String
+    let name: String
+    let title: String
+    
+    var body: some View {
+        HStack(alignment: .center, spacing: 8) {
+            AsyncImage(url: URL(string: profileImage)) { image in
+                image
+                    .resizable()
+            } placeholder: {
+                Rectangle()
+                    .foregroundStyle(Color.gray)
+            }
+            .cornerRadius(.radius12)
+            .frame(width: 36, height: 36)
+            
+            VStack(spacing: 2) {
+                Text(name)
+                    .font(.highFootnote.font)
+                    .foregroundColor(.rootStrong)
+                    .padding(.horizontal, 4)
+                
+                Text(title)
+                    .font(.highCallout.font)
+                    .padding(.horizontal, 4)
+            }
+        }
+    }
+}
