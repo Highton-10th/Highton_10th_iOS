5d86fcc8719ee3f4bdc0bdafebe4606df2911c0e
DGSWcsh <101883606+ChoiSihun@users.noreply.github.com>
2025-02-15 19:09:13 +0900

    feat: FeedBody

--- highton_10/Source/Component/Feed/Body/FeedBody.swift
+++ highton_10/Source/Component/Feed/Body/FeedBody.swift
@@ -1,8 +1,27 @@
 //
-//  Body.swift
+//  FeedBody.swift
 //  highton_10
 //
 //  Created by 최시훈 on 2/15/25.
 //
 
-import Foundation
+import SwiftUI
+
+struct FeedBody: View {
+    let imageURL: String
+    
+    var body: some View {
+        
+        AsyncImage(url: URL(string: imageURL)) { image in
+            image
+                .resizable()
+        } placeholder: {
+            Rectangle()
+                .foregroundStyle(Color.gray)
+        }
+        .cornerRadius(.radius12)
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .padding(.vertical, 8)
+        .padding(.horizontal, 16)
+    }
+}
