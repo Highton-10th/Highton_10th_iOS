a8088ca5855e7ef912bd5f7c29f6302af37e1158
“xixn2” <ssbkh0802@naver.com>
2025-02-15 19:02:55 +0900

    file delete

--- highton_10/Source/Component/Feed/Foot/FeedFoot.swift
+++ highton_10/Source/Component/Feed/Foot/FeedFoot.swift
@@ -5,6 +5,48 @@
 //  Created by 최시훈 on 2/15/25.
 //
 
-import Foundation
+import SwiftUI
 
-struct
+struct FeedFoot: View {
+    var title: String
+    var description: String
+    var percent: Int
+    var startDate: String
+    var endDate: String
+
+    public init(
+        title: String,
+        description: String,
+        percent: Int,
+        startDate: String,
+        endDate: String
+    ) {
+        self.title = title
+        self.description = description
+        self.percent = percent
+        self.startDate = startDate
+        self.endDate = endDate
+    }
+
+    var body: some View {
+        VStack {
+            Text(title)
+        }
+    }
+}
+
+struct test1: View {
+    var body: some View {
+        FeedFoot(
+            title: "뉴진스",
+            description: "뉴진스 [How Sweet] 미니 앨범 공동 구매",
+            percent: 80,
+            startDate: "1월 17일 시작",
+            endDate: "2월 27일까지"
+        )
+    }
+}
+
+#Preview {
+    test1()
+}
--- highton_10/Source/Model/GroupPurchase /ViewDefault.swift
+++
@@ -1,8 +0,0 @@
-//
-//  ViewDefault.swift
-//  highton_10
-//
-//  Created by 최시훈 on 2/15/25.
-//
-
-import Foundation
